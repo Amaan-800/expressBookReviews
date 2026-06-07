681408e4b9d28e125e9b4f5f035a3b24fa065f16
Amaan-800 <amaanmohammad1111@gmail.com>
2026-06-08 00:36:22 +0530

    Added async axios routes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios')
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -92,6 +93,66 @@ public_users.get('/review/:isbn', function (req, res) {
   const isbn = req.params.isbn;
 
   return res.status(200).json(books[isbn].reviews);
+});
+
+// Get all books using async callback
+public_users.get('/async/books', async function (req, res) {
+  try {
+    const response = await axios.get('http://localhost:5000/');
+    return res.status(200).json(response.data);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+// Get book by ISBN using Promises
+public_users.get('/async/isbn/:isbn', function (req, res) {
+
+  const isbn = req.params.isbn;
+
+  axios.get(`http://localhost:5000/isbn/${isbn}`)
+    .then(response => {
+      res.status(200).json(response.data);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+
+});
+
+// Get book by Author using Promises
+public_users.get('/async/author/:author', function (req, res) {
+
+  const author = req.params.author;
+
+  axios.get(`http://localhost:5000/author/${author}`)
+    .then(response => {
+      res.status(200).json(response.data);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+
+});
+
+// Get book by Title using async/await
+public_users.get('/async/title/:title', async function (req, res) {
+
+  try {
+
+    const title = req.params.title;
+
+    const response =
+      await axios.get(`http://localhost:5000/title/${title}`);
+
+    return res.status(200).json(response.data);
+
+  } catch (err) {
+
+    return res.status(500).json(err);
+
+  }
+
 });
 
 module.exports.general = public_users;
